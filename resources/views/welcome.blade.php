67fbdbc6244dd40b5cddd875bae13f404b2b8cae
khuzaemah <khuza.emah24@gmail.com>
2022-04-04 11:03:24 +0700

    revisi home sementara

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,17 +106,6 @@
             </div>
         </div> --}}
 
-        <!-- Slide 3 -->
-        {{-- <div class="carousel-item" style="background: url({{asset('template')}}/assets/img/slide/slide-3.jpg)">
-            <div class="carousel-container">
-                <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown">Sequi ea <span>Dime Lara</span></h2>
-                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <a href="" class="btn-get-started animate__animated animate__fadeInUp">Read More</a>
-                </div>
-            </div>
-        </div> --}}
-
     </div>
 
     <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
@@ -164,121 +153,67 @@
     </div>
 </section><!-- End Featured Section -->
 
-<!-- ======= About Section ======= -->
-<section id="about" class="about">
-    <div class="container">
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+        <div class="container">
 
-    <div class="row">
-        <div class="col-lg-6">
-        <img src="{{asset('template')}}/assets/img/about.jpg" class="img-fluid" alt="">
-        </div>
-        <div class="col-lg-6 pt-4 pt-lg-0 content">
-        <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-        <p class="fst-italic">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
-        </p>
-        <ul>
-            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-        </ul>
-        <p>
-            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-        </p>
-        </div>
-    </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+                <img src="{{asset('template')}}/assets/img/team/team-1.jpg" alt="">
+                <h4>Walter White</h4>
+                <span>Chief Executive Officer</span>
+                <p>
+                Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis quaerat qui aut aut aut
+                </p>
+                <div class="social">
+                <a href=""><i class="bi bi-twitter"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+            </div>
 
-    </div>
-</section><!-- End About Section -->
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+                <img src="{{asset('template')}}/assets/img/team/team-2.jpg" alt="">
+                <h4>Sarah Jhinson</h4>
+                <span>Product Manager</span>
+                <p>
+                Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus
+                </p>
+                <div class="social">
+                <a href=""><i class="bi bi-twitter"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+            </div>
 
-<!-- ======= Services Section ======= -->
-<section id="services" class="services">
-    <div class="container">
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+                <img src="{{asset('template')}}/assets/img/team/team-3.jpg" alt="">
+                <h4>William Anderson</h4>
+                <span>CTO</span>
+                <p>
+                Voluptas necessitatibus occaecati quia. Earum totam consequuntur qui porro et laborum toro des clara
+                </p>
+                <div class="social">
+                <a href=""><i class="bi bi-twitter"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+            </div>
 
-    <div class="row">
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-        <div class="icon-box">
-            <div class="icon"><i class="bx bxl-dribbble"></i></div>
-            <h4><a href="">Lorem Ipsum</a></h4>
-            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-        </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-        <div class="icon-box">
-            <div class="icon"><i class="bx bx-file"></i></div>
-            <h4><a href="">Sed ut perspiciatis</a></h4>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-        <div class="icon-box">
-            <div class="icon"><i class="bx bx-tachometer"></i></div>
-            <h4><a href="">Magni Dolores</a></h4>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-        </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-        <div class="icon-box">
-            <div class="icon"><i class="bx bx-world"></i></div>
-            <h4><a href="">Nemo Enim</a></h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-        </div>
         </div>
 
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-        <div class="icon-box">
-            <div class="icon"><i class="bx bx-slideshow"></i></div>
-            <h4><a href="">Dele cardo</a></h4>
-            <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
         </div>
-        </div>
+    </section><!-- End Team Section -->
 
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-        <div class="icon-box">
-            <div class="icon"><i class="bx bx-arch"></i></div>
-            <h4><a href="">Divera don</a></h4>
-            <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
-        </div>
-        </div>
-
-    </div>
-
-    </div>
-</section><!-- End Services Section -->
-
-<!-- ======= Clients Section ======= -->
-<section id="clients" class="clients">
-    <div class="container">
-
-    <div class="section-title">
-        <h2>Clients</h2>
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-    </div>
-
-    <div class="clients-slider swiper">
-        <div class="swiper-wrapper align-items-center">
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
-        <div class="swiper-slide"><img src="{{asset('template')}}/assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
-        </div>
-        <div class="swiper-pagination"></div>
-    </div>
-
-    </div>
-</section><!-- End Clients Section -->
-
-</main><!-- End #main -->
 
 <!-- ======= Top Bar ======= -->
 <section id="topbar" class="d-flex align-items-center">

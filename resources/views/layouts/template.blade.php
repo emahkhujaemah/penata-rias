eee36d1e1a2841837e3b31d1dafc748cec79618c
khuzaemah <khuza.emah24@gmail.com>
2022-04-05 13:44:49 +0700

    ganti directory asset template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -17,15 +17,15 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 <!-- Vendor CSS Files -->
-<link href="{{asset('template')}}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="{{asset('template')}}/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-<link href="{{asset('template')}}/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-<link href="{{asset('template')}}/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-<link href="{{asset('template')}}/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-<link href="{{asset('template')}}/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+<link href="{{asset('vendor')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="{{asset('vendor')}}/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="{{asset('vendor')}}/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="{{asset('vendor')}}/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="{{asset('vendor')}}/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="{{asset('vendor')}}/vendor/animate.css/animate.min.css" rel="stylesheet">
 
 <!-- Template Main CSS File -->
-<link href="{{asset('template')}}/assets/css/style.css" rel="stylesheet">
+<link href="{{asset('css')}}/style.css" rel="stylesheet">
 
 <!-- =======================================================
 * Template Name: Eterna - v4.7.0
@@ -239,16 +239,16 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="{{asset('template')}}/assets/vendor/purecounter/purecounter.js"></script>
-<script src="{{asset('template')}}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="{{asset('template')}}/assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="{{asset('template')}}/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="{{asset('template')}}/assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="{{asset('template')}}/assets/vendor/waypoints/noframework.waypoints.js"></script>
-<script src="{{asset('template')}}/assets/vendor/php-email-form/validate.js"></script>
+<script src="{{asset('vendor')}}/vendor/purecounter/purecounter.js"></script>
+<script src="{{asset('vendor')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('vendor')}}/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="{{asset('vendor')}}/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="{{asset('vendor')}}/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="{{asset('vendor')}}/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="{{asset('vendor')}}/vendor/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
-<script src="{{asset('template')}}/assets/js/main.js"></script>
+<script src="{{asset('js')}}/main.js"></script>
 
 </body>
 
